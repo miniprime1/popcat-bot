a3a94d940f035b0b4d700f718680065ef6d2cdfd
miniprime1 <64111105+miniprime1@users.noreply.github.com>
2021-08-29 13:46:35 +0900

    Update and rename popcat_bot.js to bots.js

---
+++ bots.js
@@ -0,0 +1,18 @@
+/*
+ PopCat.Click AutoClicker v1.0
+
+ <How to use>
+ (1) Open 'https://popcat.click'
+ (2) Open console (F12 or Fn+F12)
+ (3) Insert code and run
+*/
+
+var event = new KeyboardEvent('keydown', {key: 'j', ctrlKey: true})
+
+setInterval(
+    function() {
+        for (i=0; i<100; i++) {
+            document.dispatchEvent(event)
+        }
+    }
+)
